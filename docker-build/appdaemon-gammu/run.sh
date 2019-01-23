53d3e55e7593b13e1fe4a7319a6a3a7422c19347
Olivier <olivier.revelat@gmail.com>
2019-01-23 20:57:56 +0100

    Modification des compose pour correspondre aux changements hw Test pour être sûre de démarrer gammusmsd au démarrage du container appdaemon

--- docker-build/appdaemon-gammu/run.sh
+++ docker-build/appdaemon-gammu/run.sh
@@ -2,7 +2,8 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 B=$(which bash)
 
-gammu-smsd --pid=/var/run/gammu-smsd.pid --daemon
+sleep 10 
+/usr/bin/gammu-smsd --pid=/var/run/gammu-smsd.pid --daemon
 
 cd /usr/src/app
 ./dockerStart.sh
